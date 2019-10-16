36fc7692253d7ab69a933172dc3a7a445dffcd11
Ni Ngo <ni.ngo@asnet.com.vn>
2019-10-16 17:49:23 +0700

    Random dice number

--- dicee-flutter-master/lib/main.dart
+++ dicee-flutter-master/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -15,7 +16,15 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDice = 1;
+  int rightDice = 5;
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -28,13 +37,22 @@ class DicePage extends StatelessWidget {
             children: <Widget>[
               Expanded(
                 child: FlatButton(
-                  child: Image.asset('images/dice1.png'),
-                  onPressed: ,
+                  child: Image.asset('images/dice$leftDice.png'),
+                  onPressed: () {
+                    setState(() {
+                      leftDice = Random().nextInt(6) + 1; // 1 - 6
+                    });
+                  },
                 ),
               ),
               Expanded(
                 child: FlatButton(
-                  child: Image.asset('images/dice2.png'),
+                  child: Image.asset('images/dice$rightDice.png'),
+                  onPressed: () {
+                    setState(() {
+                      rightDice = Random().nextInt(6) + 1;
+                    });
+                  },
                 ),
               )
             ],
