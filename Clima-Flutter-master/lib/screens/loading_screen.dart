1dc5b0e7e4b75bdde8dd659ece6864daacdbf9b5
Ni Ngo <ni.ngo@asnet.com.vn>
2019-10-28 15:54:17 +0700

    Init clima app

---
+++ Clima-Flutter-master/lib/screens/loading_screen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+
+class LoadingScreen extends StatefulWidget {
+  @override
+  _LoadingScreenState createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+  void getLocation() async {
+    Position position = await Geolocator().getCurrentPosition(
+      desiredAccuracy: LocationAccuracy.low,
+    );
+    print(position);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getLocation();
+  }
+
+  @override
+  void deactivate() {
+    super.deactivate();
+    print('deactivate called');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
