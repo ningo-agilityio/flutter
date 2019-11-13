239a8eedd26c3891aae5b85d9ff83515cee80cdc
Ni Ngo <ni.ngo@asnet.com.vn>
2019-11-13 14:16:03 +0700

    Refactor code with round button widget

--- flash-chat-flutter-master/lib/screens/login_screen.dart
+++ flash-chat-flutter-master/lib/screens/login_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flash_chat/components/round_button.dart';
 
 class LoginScreen extends StatefulWidget {
+  static const String id = 'login_screen';
+
   @override
   _LoginScreenState createState() => _LoginScreenState();
 }
@@ -16,9 +19,12 @@ class _LoginScreenState extends State<LoginScreen> {
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            Container(
-              height: 200.0,
-              child: Image.asset('images/logo.png'),
+            Hero(
+              tag: 'logo',
+              child: Container(
+                height: 200.0,
+                child: Image.asset('images/logo.png'),
+              ),
             ),
             SizedBox(
               height: 48.0,
@@ -75,23 +81,12 @@ class _LoginScreenState extends State<LoginScreen> {
             SizedBox(
               height: 24.0,
             ),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 16.0),
-              child: Material(
-                color: Colors.lightBlueAccent,
-                borderRadius: BorderRadius.all(Radius.circular(30.0)),
-                elevation: 5.0,
-                child: MaterialButton(
-                  onPressed: () {
-                    //Implement login functionality.
-                  },
-                  minWidth: 200.0,
-                  height: 42.0,
-                  child: Text(
-                    'Log In',
-                  ),
-                ),
-              ),
+            RoundButton(
+              text: 'Log In',
+              bgColor: Colors.lightBlueAccent,
+              onPressed: () {
+                //Implement login functionality.
+              },
             ),
           ],
         ),
