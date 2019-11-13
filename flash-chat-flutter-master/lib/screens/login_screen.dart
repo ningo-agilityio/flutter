1f414ec3daf83cdcfeb970bc6a213a950028f950
Ni Ngo <ni.ngo@asnet.com.vn>
2019-11-13 16:08:26 +0700

    Add loading indicator during waiting

--- flash-chat-flutter-master/lib/screens/login_screen.dart
+++ flash-chat-flutter-master/lib/screens/login_screen.dart
@@ -3,6 +3,7 @@ import 'package:flash_chat/components/round_button.dart';
 import 'package:flash_chat/components/round_text_field.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'chat_screen.dart';
+import 'package:modal_progress_hud/modal_progress_hud.dart';
 
 class LoginScreen extends StatefulWidget {
   static const String id = 'login_screen';
@@ -14,68 +15,79 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   String email;
   String password;
+  bool isLoading = false;
   final _auth = FirebaseAuth.instance;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 24.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Hero(
-              tag: 'logo',
-              child: Container(
-                height: 200.0,
-                child: Image.asset('images/logo.png'),
+      body: ModalProgressHUD(
+        inAsyncCall: isLoading,
+        color: Colors.grey[900],
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Hero(
+                tag: 'logo',
+                child: Container(
+                  height: 200.0,
+                  child: Image.asset('images/logo.png'),
+                ),
               ),
-            ),
-            SizedBox(
-              height: 48.0,
-            ),
-            RoundTextField(
-              obscure: false,
-              inputType: TextInputType.text,
-              hintText: 'Enter your email',
-              onChanged: (value) {
-                email = value;
-              },
-            ),
-            SizedBox(
-              height: 8.0,
-            ),
-            RoundTextField(
-              obscure: false,
-              inputType: TextInputType.text,
-              hintText: 'Enter your password',
-              onChanged: (value) {
-                password = value;
-              },
-            ),
-            SizedBox(
-              height: 24.0,
-            ),
-            RoundButton(
-              text: 'Log In',
-              bgColor: Colors.lightBlueAccent,
-              onPressed: () async {
-                try {
-                  final user = _auth.signInWithEmailAndPassword(
-                    email: email,
-                    password: password,
-                  );
-                  if (user != null) {
-                    Navigator.pushNamed(context, ChatScreen.id);
+              SizedBox(
+                height: 48.0,
+              ),
+              RoundTextField(
+                obscure: false,
+                inputType: TextInputType.text,
+                hintText: 'Enter your email',
+                onChanged: (value) {
+                  email = value;
+                },
+              ),
+              SizedBox(
+                height: 8.0,
+              ),
+              RoundTextField(
+                obscure: false,
+                inputType: TextInputType.text,
+                hintText: 'Enter your password',
+                onChanged: (value) {
+                  password = value;
+                },
+              ),
+              SizedBox(
+                height: 24.0,
+              ),
+              RoundButton(
+                text: 'Log In',
+                bgColor: Colors.lightBlueAccent,
+                onPressed: () async {
+                  setState(() {
+                    isLoading = true;
+                  });
+                  try {
+                    final user = _auth.signInWithEmailAndPassword(
+                      email: email,
+                      password: password,
+                    );
+                    if (user != null) {
+                      Navigator.pushNamed(context, ChatScreen.id);
+                    }
+                    setState(() {
+                      isLoading = false;
+                    });
+                  } catch (e) {
+                    print(e);
                   }
-                } catch (e) {
-                  print(e);
-                }
-              },
-            ),
-          ],
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
--- flash-chat-flutter-master/lib/screens/registration_screen.dart
+++ flash-chat-flutter-master/lib/screens/registration_screen.dart
@@ -3,6 +3,7 @@ import 'package:flash_chat/components/round_button.dart';
 import 'package:flash_chat/components/round_text_field.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'chat_screen.dart';
+import 'package:modal_progress_hud/modal_progress_hud.dart';
 
 class RegistrationScreen extends StatefulWidget {
   static const String id = 'registration_screen';
@@ -14,69 +15,81 @@ class RegistrationScreen extends StatefulWidget {
 class _RegistrationScreenState extends State<RegistrationScreen> {
   String email;
   String password;
+  bool isLoading = false;
   final _auth = FirebaseAuth.instance;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 24.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Hero(
-              tag: 'logo',
-              child: Container(
-                height: 200.0,
-                child: Image.asset('images/logo.png'),
+      body: ModalProgressHUD(
+        inAsyncCall: isLoading,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Hero(
+                tag: 'logo',
+                child: Container(
+                  height: 200.0,
+                  child: Image.asset('images/logo.png'),
+                ),
               ),
-            ),
-            SizedBox(
-              height: 48.0,
-            ),
-            RoundTextField(
-              obscure: false,
-              inputType: TextInputType.emailAddress,
-              hintText: 'Enter your email',
-              onChanged: (value) {
-                email = value;
-              },
-            ),
-            SizedBox(
-              height: 8.0,
-            ),
-            RoundTextField(
-              obscure: true,
-              inputType: TextInputType.text,
-              hintText: 'Enter your password',
-              onChanged: (value) {
-                password = value;
-              },
-            ),
-            SizedBox(
-              height: 24.0,
-            ),
-            RoundButton(
-              text: 'Register',
-              bgColor: Colors.blueAccent,
-              onPressed: () async {
-                try {
-                  final newUser = await _auth.createUserWithEmailAndPassword(
-                    email: email,
-                    password: password,
-                  );
+              SizedBox(
+                height: 48.0,
+              ),
+              RoundTextField(
+                obscure: false,
+                inputType: TextInputType.emailAddress,
+                hintText: 'Enter your email',
+                onChanged: (value) {
+                  email = value;
+                },
+              ),
+              SizedBox(
+                height: 8.0,
+              ),
+              RoundTextField(
+                obscure: true,
+                inputType: TextInputType.text,
+                hintText: 'Enter your password',
+                onChanged: (value) {
+                  password = value;
+                },
+              ),
+              SizedBox(
+                height: 24.0,
+              ),
+              RoundButton(
+                text: 'Register',
+                bgColor: Colors.blueAccent,
+                onPressed: () async {
+                  setState(() {
+                    isLoading = true;
+                  });
 
-                  if (newUser != null) {
-                    Navigator.pushNamed(context, ChatScreen.id);
+                  try {
+                    final newUser = await _auth.createUserWithEmailAndPassword(
+                      email: email,
+                      password: password,
+                    );
+
+                    if (newUser != null) {
+                      Navigator.pushNamed(context, ChatScreen.id);
+                    }
+
+                    setState(() {
+                      isLoading = false;
+                    });
+                  } catch (e) {
+                    print(e);
                   }
-                } catch (e) {
-                  print(e);
-                }
-              },
-            ),
-          ],
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
